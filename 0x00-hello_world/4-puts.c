e2a68fd11446ee6a032a8d8b0cf7a7ab78c58db9
victorglowin <victorglowin@gmail.com>
2022-09-09 03:49:27 -0700

    solution to task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <studio.h>
+
+/**
+ * main - entry point
+ *
+ * return: always 0 (succes)
+ */
+
+int main(void)
+{
+puts ("/"Programming is like building a multilingual puzzle");
+return (0);
+}
